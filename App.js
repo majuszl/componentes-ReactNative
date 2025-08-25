6aedbae8e14954d7240a419d69d7795a63b6ca69
majuszl <167575638+majuszl@users.noreply.github.com>
2025-08-25 11:29:09 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -5,19 +5,22 @@ import Titulo from "./components/Titulo";
 export default function App() {
   const [numero, setNumero] = useState(0);
 
-  function alert() {
+  function alerta() {
       alert("Você clicou no botão")
   }
 
   return (
       <View style={styles.container}>
-        <Titulo texto={"SEJA BEM-VINDO!"} />
-        {/* BTN */}
-        <Button title={"Obrigada!"} onPress={alert} />
-      </View>
-      <View>
-          <Text>Marilsa Gomas Santieri</Text>
-          <Text>Esta é a minha página de exibição, sou uma nutricionista.</Text>
+          <Titulo texto={"SEJA BEM-VINDO!"} />
+          {/* BTN */}
+          <Button title={"Obrigada!"} onPress={alerta} />
+          <View>
+              <View style={styles.container}>
+                  <Image source={{ uri: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos%2Fnutricionista-desenho&psig=AOvVaw0U9Tybp5K8ejyl_Edg_8aa&ust=1756210032432000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjL_6z2pY8DFQAAAAAdAAAAABAE' }}/>
+              </View>
+              <Text>Marilsa Gomes Santieri</Text>
+              <Text>Esta é a minha página de exibição, sou uma nutricionista.</Text>
+          </View>
       </View>
   )
 }
